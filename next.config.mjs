669cc5f8d9941e5839f6a7833ddbfd0a9ccec731
jawad178-design <jawad.khaled@st.ul.edu.lb>
2025-07-15 18:51:43 +0300

    Simplify deployment config and add Vercel support as alternative

--- next.config.mjs
+++ next.config.mjs
@@ -10,16 +10,6 @@ const nextConfig = {
     unoptimized: true,
   },
   serverExternalPackages: ['@prisma/client', 'prisma'],
-  output: 'standalone',
-  outputFileTracingIncludes: {
-    '/api/**/*': ['./node_modules/**/*.wasm', './node_modules/**/*.node'],
-  },
-  webpack: (config, { isServer }) => {
-    if (isServer) {
-      config.externals.push('@prisma/client')
-    }
-    return config
-  },
 }
 
 export default nextConfig
